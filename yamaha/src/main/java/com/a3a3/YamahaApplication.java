2ce25389370498bae8935c1f3904781f47e7944a
AhnSaeChan <sechan8896@gmail.com>
2025-02-27 12:05:19 +0900

    20250227

---
+++ yamaha/src/main/java/com/a3a3/YamahaApplication.java
@@ -0,0 +1,13 @@
+package com.a3a3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamahaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamahaApplication.class, args);
+	}
+
+}
---
+++ yamaha/src/test/java/com/a3a3/YamahaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a3a3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamahaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
